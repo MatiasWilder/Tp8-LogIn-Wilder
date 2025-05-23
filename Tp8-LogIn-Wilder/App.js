06abe83175476cadc51b5d237b9aacda6a57232e
Android Studio <devandroid@ort>
2025-05-23 12:14:09 -0300

    primera clase

--- Tp8-LogIn-Wilder/App.js
+++ Tp8-LogIn-Wilder/App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
+    <StatusBar style="light" />
+    <SafeAreaView style={{flex: 4, justifyContent: "center", paddingHorizontal: 20}}>
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Text style={tema ? styles.texto}>Olvidaste la clave?</Text>
+      <Text style={tema ? styles.texto }>Crear cuenta</Text>
     </View>
-  );
+  
+  </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +23,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  texto: {fontFamily: "sans-serif", fontStyle: "bold", fontSize: 10},
+
 });
+
