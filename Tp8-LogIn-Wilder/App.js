fa132df4e22ace52697d315131f9d3065c228780
Android Studio <devandroid@ort>
2025-05-26 09:00:39 -0300

    clase 26/5

--- Tp8-LogIn-Wilder/App.js
+++ Tp8-LogIn-Wilder/App.js
@@ -1,18 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, ImageBackground, Button } from 'react-native';
+
 
 export default function App() {
-  return (
-    <StatusBar style="light" />
-    <SafeAreaView style={{flex: 4, justifyContent: "center", paddingHorizontal: 20}}>
+  return (<>
+    <StatusBar style="auto" />
+    <SafeAreaView style={{flex: 1, justifyContent: "center", paddingHorizontal: 20}}>
     
     <View style={styles.container}>
-
-      <Text style={tema ? styles.texto}>Olvidaste la clave?</Text>
-      <Text style={tema ? styles.texto }>Crear cuenta</Text>
+      <ImageBackground source={require('./assets/CocaCola.png')} resizeMode="cover" style={styles.image} />
+      <TextInput style={styles.input}  placeholder='Ingrese su correo'/>
+      <TextInput style={styles.input}  placeholder='Ingrese su contraseña'/>
+      <Button title='Enviar' style={styles.boton}/>
+      <Text style={styles.texto}>¿Olvidaste la clave?</Text>
+      <Text style={styles.texto}>Crear cuenta</Text>
     </View>
   
-  </SafeAreaView>
+  </SafeAreaView></>
     );
 }
 
@@ -24,7 +28,34 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
-  texto: {fontFamily: "sans-serif", fontStyle: "bold", fontSize: 10},
+  texto: {
+    fontFamily: "sans-serif",
+    fontStyle: "bold", 
+    fontSize: 10,
+    alignItems: 'center'
+  },
+
+  input:{
+    height: 40,
+    borderWidth: 1,
+    padding: 3,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  image:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 50,
+    width: 100,
+    height: 60
+  },
+
+  boton:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+  }
 
 });
-
