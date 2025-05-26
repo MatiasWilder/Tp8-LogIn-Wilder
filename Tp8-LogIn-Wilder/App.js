d48eb44b209808220c6c2bd6af80398d38a8be64
Android Studio <devandroid@ort>
2025-05-26 10:36:07 -0300

    segunda clase 26/5

--- Tp8-LogIn-Wilder/App.js
+++ Tp8-LogIn-Wilder/App.js
@@ -5,14 +5,14 @@ import { StyleSheet, Text, View, SafeAreaView, TextInput, ImageBackground, Butto
 export default function App() {
   return (<>
     <StatusBar style="auto" />
-    <SafeAreaView style={{flex: 1, justifyContent: "center", paddingHorizontal: 20}}>
+    <SafeAreaView style={{flex: 2, justifyContent: "center", paddingHorizontal: 20, backgroundColor: "#db1c12"}}>
     
     <View style={styles.container}>
-      <ImageBackground source={require('./assets/CocaCola.png')} resizeMode="cover" style={styles.image} />
-      <TextInput style={styles.input}  placeholder='Ingrese su correo'/>
-      <TextInput style={styles.input}  placeholder='Ingrese su contraseña'/>
+      <ImageBackground source={require('./assets/CocaCola.png')} resizeMode="contain" style={styles.image} />
+      <TextInput style={styles.input} type="email" placeholder='Ingrese su correo'/>
+      <TextInput style={styles.input} type="hidden" placeholder='Ingrese su contraseña'/>
       <Button title='Enviar' style={styles.boton}/>
-      <Text style={styles.texto}>¿Olvidaste la clave?</Text>
+      <Text style={styles.texto}>Olvidaste la clave?</Text>
       <Text style={styles.texto}>Crear cuenta</Text>
     </View>
   
@@ -22,21 +22,26 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 0.75,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: "#ffffff"
   },
 
+
   texto: {
+    margin: 10,
     fontFamily: "sans-serif",
     fontStyle: "bold", 
-    fontSize: 10,
-    alignItems: 'center'
+    fontSize: 14,
+    alignItems: 'center',
+    color: "#1567eb"
   },
 
   input:{
+    margin: 5,
     height: 40,
+    width: 250,
     borderWidth: 1,
     padding: 3,
     justifyContent: 'center',
@@ -48,14 +53,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     marginTop: 50,
-    width: 100,
-    height: 60
+    width: 200,
+    height: 200
   },
 
   boton:{
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 10,
+    margin: 15,
+    backgroundColor: "#ffffff",
+    textalign:'center'
   }
 
 });
